aaa5e8b81fa4f216a81443cb35158e5421dd8ba0
Abdelrahman Mohamed <obada.semary@gmail.com>
2020-09-13 03:31:09 +0300

    Initial Commit

---
+++ ORestaurants/ORestaurantsApp.swift
@@ -0,0 +1,17 @@
+//
+//  ORestaurantsApp.swift
+//  ORestaurants
+//
+//  Created by Abdelrahman Mohamed on 13.09.2020.
+//
+
+import SwiftUI
+
+@main
+struct ORestaurantsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
